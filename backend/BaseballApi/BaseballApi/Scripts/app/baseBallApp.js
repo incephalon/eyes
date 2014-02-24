59bbca7ad94d8493633857ec93585beaca8848d6
levenleven <levenstein.aleksey@gmail.com>
2014-02-24 12:22:30 -0800

    last changes

--- backend/BaseballApi/BaseballApi/Scripts/app/baseBallApp.js
+++ backend/BaseballApi/BaseballApi/Scripts/app/baseBallApp.js
@@ -11,12 +11,12 @@ baseballApp.controller("statisticsController",
         $scope.year = 2013;
         $scope.player = null;
 
-        $scope.loadPlayers = function (team) {
+        $scope.loadPlayers = function (id) {
             $scope.player = null;
             $scope.year = 2013;
             $scope.players = [];
             $scope.filteredStatistics = [];
-            $scope.players = dataService.getPlayers(team.id);
+            $scope.players = dataService.getPlayers(id);
         };
 
         $scope.isSelectedPlayer = function (player) {
@@ -48,6 +48,41 @@ baseballApp.controller("statisticsController",
                 $scope.filteredStatistics = dataService.getBattingStatistics($scope.player.teamPlayerId, $scope.year);
             }
         };
+
+
+        $scope.teams = [
+            { id: 1, name: "HOU" },
+            { id: 2, name: "MIA" },
+            { id: 3, name: "SDP" },
+            { id: 4, name: "NYM" },
+            { id: 5, name: "STL" },
+            { id: 6, name: "CHC" },
+            { id: 7, name: "SEA" },
+            { id: 8, name: "CHW" },
+            { id: 11, name: "COL" },
+            { id: 12, name: "PIT" },
+            { id: 13, name: "PHI" },
+            { id: 14, name: "LAA" },
+            { id: 15, name: "NYY" },
+            { id: 17, name: "MIN" },
+            { id: 18, name: "CIN" },
+            { id: 19, name: "SFG" },
+            { id: 20, name: "LAD" },
+            { id: 21, name: "TOR" },
+            { id: 22, name: "TBR" },
+            { id: 23, name: "ARI" },
+            { id: 25, name: "KCR" },
+            { id: 26, name: "WSN" },
+            { id: 27, name: "CLE" },
+            { id: 28, name: "TEX" },
+            { id: 30, name: "DET" },
+            { id: 31, name: "OAK" },
+            { id: 32, name: "MIA" },
+            { id: 33, name: "MIL" },
+            { id: 34, name: "BOS" },
+            { id: 36, name: "BAL" },
+            { id: 37, name: "ATL" }
+        ];
 
         $timeout(function() {
             $scope.teams = dataService.getTeams();
